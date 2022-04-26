d4e71f05d5456c91e981a02256c2f87d450dc4b0
FloLys <4440@holbertonschool.com>
2022-04-26 12:05:37 -0700

    6 corrected

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,9 +2,9 @@
 flag = False
 for i in range(10):
     for j in range(i+1, 10):
-        if flag == True:
+        if flag:
             print(", ", end='')
-        else: 
+        else:
             flag = True
         print("{}{}".format(i, j), end='')
-print('\n')
+print()
