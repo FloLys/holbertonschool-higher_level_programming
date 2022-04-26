0647fa3afb63c301b7eb7d500fd618cc14e692b0
FloLys <4440@holbertonschool.com>
2022-04-26 11:05:10 -0700

    1 and 5 corrected

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    print("{:02d}".format(i), end=", ")
     if i == 99:
-        print("99")
+        print(99)
+    else:
+        print("{:02d}".format(i), end=", ")
