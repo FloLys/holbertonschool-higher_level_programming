65e3e12a93defdf64bfd476eb0947d177e079024
FloLys <4440@holbertonschool.com>
2022-04-28 17:36:24 -0700

    0 update exec

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-sum = a+b
-print(f"{a} + {b} = {sum}")
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print(f"{a} + {b} = {add(a, b)}")
