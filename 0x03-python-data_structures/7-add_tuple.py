7c4bd765ed257ad71db67676ac1aa8522bc88a41
FloLys <4440@holbertonschool.com>
2022-05-03 12:14:41 -0700

    7 correct

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,10 +4,16 @@ def add_tuple(tuple_a=(), tuple_b=()):
     for i, elem in enumerate(tuple_a):
         if not elem:
             continue
+        if i >= 2:
+            break
         element[i] += tuple_a[i]
+
     for i, elem in enumerate(tuple_b):
         if not elem:
             continue
+        if i >= 2:
+            break
         element[i] += tuple_b[i]
+
     new_tuple = (element[0], element[1])
     return new_tuple
