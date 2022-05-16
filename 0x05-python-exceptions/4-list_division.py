0ae68b10abd041b57b4c3dcf7c5d3c8b38740bbf
FloLys <4440@holbertonschool.com>
2022-05-16 15:20:09 -0700

    4 pycode

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,7 +4,7 @@ def list_division(my_list_1, my_list_2, list_length):
     for i in range(list_length):
         try:
             div = my_list_1[i] / my_list_2[i]
-            div_list.append(div) 
+            div_list.append(div)
             continue
         except ZeroDivisionError:
             div_list.append(0)
