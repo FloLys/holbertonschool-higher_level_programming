da2e7837b0b6d41716e6738d94b5dbc787da38e9
FloLys <4440@holbertonschool.com>
2022-05-17 12:47:36 -0700

    upload

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Square:
+    """class square"""
+    def __init__(self, size):
+    """define private size with size"""
+        self.__size = size
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+i'm obssesed with vitrales lately, i just can't its damn glorious like being a spirit of light
