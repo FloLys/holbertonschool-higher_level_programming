33687da224e6f7da20e46965977495dd65b3302e
FloLys <4440@holbertonschool.com>
2022-05-17 12:56:50 -0700

    correct style

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-"""empty class"""
+"""create an empty class"""
+
+
 class Square:
+    """empty"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""define private size with size"""
+"""define size of a square by its input"""
+
+
 class Square:
+    """create square class"""
     def __init__(self, size):
+        """define private size with size"""
         self.__size = size
