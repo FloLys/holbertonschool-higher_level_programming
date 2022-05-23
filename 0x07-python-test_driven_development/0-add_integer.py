c28416542955dab660970166f2a4ac725c70b2bf
FloLys <4440@holbertonschool.com>
2022-05-23 10:05:12 -0700

    uplaod dir and 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Adds two integers"""
+
+
+def add_integer(a, b=98):
+    """Returns the addition of a and b"""
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    if not isinstance(a, int):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int):
+        raise TypeError("b must be an integer")
+    return (a + b)
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+why it's so satisfying to chew the sides/corners/edges of perfect things? like the triangle edge of a chambuchito
