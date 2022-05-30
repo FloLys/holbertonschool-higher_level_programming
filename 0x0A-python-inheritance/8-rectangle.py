65dbac970e89a851df749dfed16bba8ae184b37f
FloLys <4440@holbertonschool.com>
2022-05-30 15:58:14 -0700

    8 import for fn

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,20 +2,7 @@
 """ Rectangle module """
 
 
-class BaseGeometry():
-    """ Class """
-
-    def area(self):
-        """raises an Exception message """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ validates value, name is always a string """
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """ Subclass """
