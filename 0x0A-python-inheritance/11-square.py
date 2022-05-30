c8e929fa3d15bed8c45e8effa53cc8416ea2cc8d
FloLys <4440@holbertonschool.com>
2022-05-30 16:51:09 -0700

    correct and upload

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Rectangle module """
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Subclass """
+
+    def __init__(self, size):
+        """ instantiation """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """ area = width * height """
+        return (self.__size * self.__size)
+
+    def __str__(self):
+        """ rectangle description """
+        return (f"[Square] {self.__size}/{self.__size}")
