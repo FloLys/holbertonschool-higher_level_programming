41cf295bd7cc07aeca697ba2efbd16193d900377
FloLys <4440@holbertonschool.com>
2022-05-31 15:35:31 -0700

    correct json

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import json
 """ From JSON string to object """
+import json
 
 
 def from_json_string(my_str):
     """ Returns an object represented by JSON string """
+
     return json.loads(my_str)
