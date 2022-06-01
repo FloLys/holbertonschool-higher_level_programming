8bfb2e4b1bb518a5755bea33d80ff0454f296128
FloLys <4440@holbertonschool.com>
2022-05-31 19:45:10 -0700

    upload

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Student to JSON """
+
+
+class Student():
+    """ Class """
+
+    def __init__(self, first_name, last_name, age):
+        """ Instantiation """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Returns dict description with simple data structure """
+        if attrs is None:
+            return self.__dict__
+        else:
+            dic = {}
+            for elem in attrs:
+                if elem in self.__dict__.keys() and isinstance(elem, str):
+                    dic[elem] = self.__dict__[elem]
+            return dic
--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 """ Class to JSON """
-import json
 
 
 def class_to_json(obj):
     """ returns the dict description with simple data structure for json obj"""
-
-    json_string = json.dumps(obj.__dict__)
-    return json.loads(json_string)
+    return obj.__dict__
