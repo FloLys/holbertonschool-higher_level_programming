74f5cbd92ac38bbd477d77930086017309e2b795
FloLys <4440@holbertonschool.com>
2022-06-03 16:14:28 -0700

    update to task 12

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+""" Module Square """
+from models.base import Base
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """ Square """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Initializator """
+
+        super().__init__(size, size, x, y, id)
+
+    def area(self):
+        """ Area = size * size """
+        return self.size * self.size
+
+    def display(self):
+        """ Prints the square with # """
+        print('\n' * self.y, end="")
+#        for i in range(self.__height):
+#            print(' ' * self.__x, end="")
+#            print('#' * self.__width)
+        print(f"{(' ' * self.x)}{'#' * self.size}\n" * self.size, end="")
+
+    def __str__(self):
+        """ Print string method """
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
+
+    def update(self, *args, **kwargs):
+        """ Updates arguments """
+        attr = ["id", "size", "x", "y"]
+        if args:
+            for i in range(len(args)):
+        # setattr creates or references the attr with the name string given
+        # object.name = self."id" = self.id
+                setattr(self, attr[i], args[i])
+        else:
+            for key in kwargs:
+                setattr(self, key, kwargs[key])
+
+    @property
+    def size(self):
+        """ Size Getter """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ Size Setter """
+        self.width = value
+        self.height = value
