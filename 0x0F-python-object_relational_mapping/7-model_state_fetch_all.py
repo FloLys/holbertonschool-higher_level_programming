dda0af116c683b9833ebd7c17ef3cdca1de11d87
FloLys <4440@holbertonschool.com>
2022-08-07 15:14:06 -0700

    task 7 with alchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-""" All cities by state """
+""" All states via SQLAlchemy """
 
 
 import MySQLdb
 from sys import argv
 from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states\
-                ORDER BY states.id")
-    table = cur.fetchall()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    table = session.query(State).all()
 
     try:
-        for row in table:
-            print(f"{row[0]}: {row[1]}")
+        for state in table:
+            print(f"{state.id}: {state.name}")
     except Exception:
         print()
-
-    cur.close()
-    db.close()
