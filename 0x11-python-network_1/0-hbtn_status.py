3fd1d976c5ab26f6ab97b2a341b052ffc96db5ea
FloLys <4440@holbertonschool.com>
2022-08-19 13:52:09 -0700

    test

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""  """
+""" Fetch an url """
+import urllib.request
 
 
 if __name__ == "__main__":
-    import urllib.request
-
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body response:")
