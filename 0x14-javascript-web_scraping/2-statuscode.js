d9be96e2c078a11fb9769cdf50501271a379f471
FloLys <florelysmestre@gmail.com>
2022-09-13 17:26:21 +0000

    tasks

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const axios = require('axios');
+
+axios.get(process.argv[2], 'utf8')
+  .then(response => {
+    console.log('code: ', response.status);
+  })
+  .catch(error => {
+    console.log('code: ', error.response.status);
+  });
