01d3726b5b91c9125d5792bebd04c0c8f923e2a2
FloLys <4440@holbertonschool.com>
2022-09-21 13:57:20 -0300

    task8

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,6 +1,6 @@
 const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
 $.get(url, function (data) {
-	$.each(data, function (index, item) {
+	$.each(data.results, function (index, item) {
 		$('UL#list_movies').append('<li>' + item.title + '</li>');
 	})
 });
